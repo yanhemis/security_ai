7eb565e020ff9a6bf3843ceee366932de822c3b1
yanhemis <87313463+yanhemis@users.noreply.github.com>
2025-05-21 22:35:06 +0900

    Create 01_keyword_detector.py

---
+++ security_ai_week1/security_ai_week1/01_keyword_detector.py
@@ -0,0 +1,11 @@
+python
+
+def is_malicious(text, keyword_list):
+    for keyword in keyword_list:
+        if keyword in text.lower():
+            return True
+    return False
+
+malicious_keywords = ["sql injection", "xss", "rce"]
+text = "SQL Injection 공격이 있습니다."
+print("탐지 결과:", is_malicious(text, malicious_keywords))
